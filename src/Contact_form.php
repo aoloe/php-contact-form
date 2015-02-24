8736d376ea54cff885515b5d8a50a8ffcae84bbb
Ale Rimoldi <ale@graphicslab.org>
2015-02-25 00:16:21 +0100

    reindent and add a force smtp option

--- src/Contact_form.php
+++ src/Contact_form.php
@@ -2,7 +2,14 @@
 
 namespace Aoloe;
 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 class Contact_form {
+
+    private $force_smtp = false;
+    public function set_force_smtp($force = true) {$this->force_smtp = $force;}
 
     private $request_prefix = "contact_form_";
     public function set_request_prefix($prefix) {$this->request_prefix = $prefix;}
@@ -55,6 +62,9 @@ class Contact_form {
 
     private $subject = null;
     public function set_subject($subject) {$this->subject = $subject;}
+
+    private $message = "";
+    public function set_message($message) {$this->message = $message;} // TODO: do we really need both message and contetn?
 
     private $content = null;
     public function set_content($content) {$this->content = $content;}
@@ -121,83 +131,89 @@ class Contact_form {
             $this->field_valid_email[$key] = (empty($this->field[$key]) || (strpos($this->field[$key], '@') !== false));
             $result &= $this->field_valid_email[$key];
         }
-    // debug('field_valid_email', $this->field_valid_email);
-    return $result;
-}
-
-public function is_spam() {
-    $result = false;
-    // TODO: eventually add an honoey pot field that must be left empty...
-    $result &= !array_key_exists('subject', $this->field_request) || (strpos($this->field['subject'], "MIME-Version") !== false);
-    $result &= !array_key_exists('subject', $this->field_request) || (strpos($this->field['subject'], "Content-Type") !== false);
-    $result &= !array_key_exists('email', $this->field_request) || (strpos($this->field['email'], "MIME-Version") !== false);
-    $result &= !array_key_exists('message', $this->field_request) || (strpos($this->field['message'], "MIME-Version") !== false);
-    if (array_key_exists('email', $this->field) && array_key_exists('subject', $this->field)) {
-        $result &= ($this->field['email'] == $this->field['subject']);
+        // debug('field_valid_email', $this->field_valid_email);
+        return $result;
     }
-    if (array_key_exists('email', $this->field) && array_key_exists('message', $this->field)) {
-        $result &= ($this->field['email'] == $this->field['message']);
-    }
-    return $result;
-}
 
-public function fill_message() {
-    $this->content = "";
-    foreach($this->field as $key => $value) {
-        if (!empty($value)) {
-            if ($key == 'file') {
-                $value = $value['name'];
-            }
-            $this->content .= str_replace("_", " ", $key).": ".$value."\n";
+    public function is_spam() {
+        $result = false;
+        // TODO: eventually add an honoey pot field that must be left empty...
+        $result &= !array_key_exists('subject', $this->field_request) || (strpos($this->field['subject'], "MIME-Version") !== false);
+        $result &= !array_key_exists('subject', $this->field_request) || (strpos($this->field['subject'], "Content-Type") !== false);
+        $result &= !array_key_exists('email', $this->field_request) || (strpos($this->field['email'], "MIME-Version") !== false);
+        $result &= !array_key_exists('message', $this->field_request) || (strpos($this->field['message'], "MIME-Version") !== false);
+        if (array_key_exists('email', $this->field) && array_key_exists('subject', $this->field)) {
+            $result &= ($this->field['email'] == $this->field['subject']);
         }
+        if (array_key_exists('email', $this->field) && array_key_exists('message', $this->field)) {
+            $result &= ($this->field['email'] == $this->field['message']);
+        }
+        return $result;
     }
-}
 
-function send($server = "") {
-    $result = true;
-    // debug('field', $this->field);
-    if (empty($this->field_request_file)) {
-        $result = mail(
-            $this->mail_to,
-            $this->subject_prefix.$this->field['subject'],
-            $this->field['message'],
-            "From: ".$this->field['email']
-        );
-    } else {
-        // debug('field_request_file', $this->field_request_file);
-        $mail = new \PHPMailer();
-        foreach ($this->field_request_file as $item) {
-            // debug('field[key]', $this->field[$item]);
-            if (!empty($this->field[$item])) {
-                $mail->AddAttachment($this->field[$item]['location'], $this->field[$item]['name'], 'base64', $this->field[$item]['type']);
+    public function fill_message() {
+        $this->content = "";
+        foreach($this->field as $key => $value) {
+            if (!empty($value)) {
+                if ($key == 'file') {
+                    $value = $value['name'];
+                }
+                $this->content .= str_replace("_", " ", $key).": ".$value."\n";
             }
         }
-        $mail->addAddress($this->mail_to);
-        $mail->setFrom(array_key_exists('email', $this->field) ? $this->field['email'] : $this->mail_from);
-        $mail->subject = $this->subject_prefix.' '.(isset($this->subject) ? $this->subject : '');
-        if (isset($this->content)) {
-            $mail->Body = $this->content;
+    }
+
+    function send($server = "") {
+        $result = true;
+        // debug('field', $this->field);
+        if (!$this->force_smtp && empty($this->field_request_file)) {
+            $result = mail(
+                $this->mail_to,
+                $this->subject_prefix. (array_key_exists('subject', $this->field) ? $this->field['subject'] : $this->subject),
+                array_key_exists('message', $this->field) ? $this->field['message'] : $this->message,
+                "From: ".$this->field['email']
+            );
+            // debug('mail() result', $result);
         } else {
-            $body = array();
-            foreach ($this->field_request as $key => $value) {
-                $body[] = $key.': '.$this->field[$key];
+            // debug('field_request_file', $this->field_request_file);
+            $mail = new \PHPMailer();
+            $mail->SMTPDebug  = 2;
+            foreach ($this->field_request_file as $item) {
+                // debug('field[key]', $this->field[$item]);
+                if (!empty($this->field[$item])) {
+                    $mail->AddAttachment($this->field[$item]['location'], $this->field[$item]['name'], 'base64', $this->field[$item]['type']);
+                }
             }
-            $mail->Body = implode("\n\n", $body);
+            $mail->addAddress($this->mail_to);
+            $mail->setFrom(array_key_exists('email', $this->field) ? $this->field['email'] : $this->mail_from);
+            $mail->Subject = $this->subject_prefix.' '.(isset($this->subject) ? $this->subject : '');
+            $mail->CharSet = 'UTF-8';
+            if (isset($this->content)) {
+                $mail->Body = $this->content;
+            } else {
+                $body = array();
+                // debug('field_request', $this->field_request);
+                // debug('field', $this->field);
+                foreach ($this->field_request as $key => $value) {
+                    $body[] = $key.': '.$this->field[$key];
+                }
+                $mail->Body = implode("\n\n", $body);
+            }
+            // debug('body', $mail->Body);
+            $result = $mail->send();
+            // debug('result', $result);
+            // debug('ErrorInfo', $mail->ErrorInfo);
+            /*
+            $attachment = $mail->getFile($this->field['file']['location']);
+            $mail->addAttachment($attachment, $this->field['file']['name'], $this->field['file']['type']);
+            $mail->setFrom($this->field['email']);
+            $mail->setSubject($this->subject_prefix.$this->field['subject']);
+            $mail->setText($this->content);
+            $mail->setHeader('X-Mailer', 'HTML Mime mail class (http://www.phpguru.org)');
+            $result = $mail->send(array($this->target));
+            */
         }
-        // debug('body', $mail->Body);
-        $result = $mail->send();
         // debug('result', $result);
-        // debug('ErrorInfo', $mail->ErrorInfo);
-        /*
-        $attachment = $mail->getFile($this->field['file']['location']);
-        $mail->addAttachment($attachment, $this->field['file']['name'], $this->field['file']['type']);
-        $mail->setFrom($this->field['email']);
-        $mail->setSubject($this->subject_prefix.$this->field['subject']);
-        $mail->setText($this->content);
-        $mail->setHeader('X-Mailer', 'HTML Mime mail class (http://www.phpguru.org)');
-        $result = $mail->send(array($this->target));
-        */
+        return $result;
     }
-    return $result;
-}
 }
